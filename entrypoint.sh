ec8f38f6b2f2071ea96f2294f1a16bf8cd8df286
Derek Honerlaw <honerlawd@gmail.com>
2026-05-17 12:00:21 -0400

    chore: run db upgrade with DATABASE_ADMIN_URL if set
    Allows migrations to run as a privileged user (e.g. doadmin) while the
    app process continues using a lower-privileged DATABASE_URL. Needed on
    managed Postgres 15+ where the app user lacks CREATE on public schema.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-flask --app wsgi db upgrade
+DATABASE_URL="${DATABASE_ADMIN_URL:-$DATABASE_URL}" flask --app wsgi db upgrade
 exec gunicorn wsgi:app --bind 0.0.0.0:8080 --workers 1 --timeout 120
